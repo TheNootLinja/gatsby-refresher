2514ef8618787ebee82975a6f26b690720da9865
TheNootLinja <thenootlinja@gmail.com>
2022-01-23 12:53:42 -0500

    Add blog page, 3 test blogs, and configure gatsby-source-filesystem to pull blog posts from blog folder

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,24 +1,54 @@
 import React from 'react';
-import { Link } from 'gatsby';
-import { container, heading, navLinks, navLinkItem, navLinkText } from './layout.module.css';
-import '../styles/global.css'
+import { Link, useStaticQuery, graphql } from 'gatsby';
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+  siteTitle,
+} from './layout.module.css';
+import '../styles/global.css';
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
   return (
     <div className={container}>
-        <title>{pageTitle}</title>
-        <nav>
-            <ul className={navLinks}>
-                <li className={navLinkItem}><Link className={navLinkText} to="/">Home</Link></li>
-                <li className={navLinkItem}><Link className={navLinkText} to="/about">About</Link></li>
-            </ul>
-        </nav>
-        <main>
-            <h1 className={heading}>{pageTitle}</h1>
-            {children}
-        </main>
+      <title>{pageTitle}</title>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link className={navLinkText} to="/">
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link className={navLinkText} to="/blog">
+              Blog
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link className={navLinkText} to="/about">
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
     </div>
-  )
+  );
 };
 
 export default Layout;
---
+++ src/pages/blog.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const BlogPage = () => {
+  return <Layout>
+      <p>My cool blog posts will go here!</p>
+  </Layout>;
+};
+
+export default BlogPage;
