41076561814c89360eae88da98b432395b8822f5
TheNootLinja <thenootlinja@gmail.com>
2022-01-23 13:21:09 -0500

    Add query to BlogPage component to pull in the names of all files in the blog folder, then display them on the page.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
+
   return <Layout>
-      <p>My cool blog posts will go here!</p>
+    <ul>
+      {
+        data.allFile.nodes.map(node => (
+          <li key={node.name}>
+            {node.name}
+          </li>
+        ))
+      }
+    </ul>
   </Layout>;
 };
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
 
 export default BlogPage;
