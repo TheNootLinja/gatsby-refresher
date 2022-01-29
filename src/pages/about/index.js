f21157b9977312b2c2df0fc4ffbb3898491eb893
TheNootLinja <thenootlinja@gmail.com>
2022-01-29 12:54:29 -0500

    Style headings of blog posts in list and start working on home and about content

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -5,7 +5,13 @@ const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
       <div className="about-container">
-        <p>Need to contact A Pretty Cool Blog LLC?</p>
+        {/* Need photo for top of this page */}
+        <p className="about-content">
+          Pretty Cool Blog focuses on nightlife in the city ranging from cool
+          neon photography to the best hidden gems to eat at. Whether you are
+          looking to get ideas for activities in the city at night or just want
+          to live through us, we think we can help!
+        </p>
       </div>
     </Layout>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,22 @@ import * as React from 'react';
 import Layout from '../components/layout';
 import Footer from '../components/footer/footer'
 import { StaticImage } from 'gatsby-plugin-image';
+import { homepageImg, contentContainer } from './index.module.css';
 
 const IndexPage = () => {
   return (
       <div className="index-page">
         <Layout pageTitle="Home Page">
-          <p>
-            I'm baby letterpress umami asymmetrical, occupy lomo gochujang flannel
-            ugh swag semiotics 3 wolf moon. Shabby chic aesthetic iceland pork belly
-            enamel marfa blog single-origin coffee.
-          </p>
           <StaticImage
-            className="homepage-img"
+            className={homepageImg}
             alt="Picture of my 2018 Colorado ZR2"
             src="../images/neoncity.jpeg"
-          />
+            />
+          <div className={contentContainer}>
+            <p>
+                Welcome to the coolest blog on the internet (or at least a Pretty Cool one!).
+            </p>
+          </div>
         </Layout>
       </div>
   );
